0e0632ea594f55a0b42bb3839539ef850238e46c
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-03 10:12:21 -0300

    chore: setting up the project

---
+++ src/test/java/com/luxkapotter/urubupix/UrubuPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.urubupix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubuPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
