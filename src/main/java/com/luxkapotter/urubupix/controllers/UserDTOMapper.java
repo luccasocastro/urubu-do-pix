b32f17c3bc9e5217775f78a4d9ef015d76bb2233
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-03 21:10:52 -0300

    fix: adding the annotation @Component

--- src/main/java/com/luxkapotter/urubupix/controllers/UserDTOMapper.java
+++ src/main/java/com/luxkapotter/urubupix/controllers/UserDTOMapper.java
@@ -1,9 +1,12 @@
 package com.luxkapotter.urubupix.controllers;
 
+import org.springframework.stereotype.Component;
+
 import com.luxkapotter.urubupix.models.User;
 import com.luxkapotter.urubupix.models.DTOs.UserRequest;
 import com.luxkapotter.urubupix.models.DTOs.UserResponse;
 
+@Component
 public class UserDTOMapper {
     
     public UserResponse toResponse(User user){
