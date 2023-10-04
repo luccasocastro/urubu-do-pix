97a8641e1a54ce27c4322383c53dee0240f861b4
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-03 21:12:04 -0300

    refact: updating investment controller

--- src/main/java/com/luxkapotter/urubupix/controllers/InvestmentController.java
+++ src/main/java/com/luxkapotter/urubupix/controllers/InvestmentController.java
@@ -1,7 +1,5 @@
 package com.luxkapotter.urubupix.controllers;
 
-import java.time.LocalDate;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,32 +7,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luxkapotter.urubupix.models.Investment;
-import com.luxkapotter.urubupix.models.User;
 import com.luxkapotter.urubupix.models.DTOs.InvestmentRequest;
 import com.luxkapotter.urubupix.models.DTOs.UserResponse;
-import com.luxkapotter.urubupix.repositories.InvestmentRepository;
 import com.luxkapotter.urubupix.repositories.UserRepository;
+import com.luxkapotter.urubupix.services.InvestmentService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/investments")
 public class InvestmentController {
-    private InvestmentRepository investmentRepository;
-    private UserRepository userRepository;
+    private final InvestmentService investmentService;
+    private final UserRepository userRepository;
+    private final UserDTOMapper userDTOMapper;
 
-    public InvestmentController(InvestmentRepository investmentRepository, UserRepository userRepository) {
-        this.investmentRepository = investmentRepository;
+    public InvestmentController(UserRepository userRepository, InvestmentService investmentService, UserDTOMapper userDTOMapper) {
         this.userRepository = userRepository;
+        this.investmentService = investmentService;
+        this.userDTOMapper = userDTOMapper;
     }
 
     @PostMapping("/{user_id}")
     public ResponseEntity<UserResponse> createInvestment(@RequestBody @Valid InvestmentRequest obj, @PathVariable Long user_id){
-        User user = userRepository.findById(user_id).get();
-        Investment deposit = new Investment(obj.value(), LocalDate.now(), LocalDate.now().plusDays(30), user);
-        investmentRepository.save(deposit);
-        return ResponseEntity.ok(new UserResponse(user_id, user.getName(), user.getEmail(), user.getInvestments()));
+        investmentService.create(obj, user_id);
+        return ResponseEntity.ok(userDTOMapper.toResponse(userRepository.findById(user_id).get()));
     }
     
 }
